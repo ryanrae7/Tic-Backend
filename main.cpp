80f3cfb34bc26e55a41811c0794138e30e32a7d9
Ryan Gae <ryangae7@gmail.com>
2024-10-12 18:06:49 -0400

    Second update (10/12/2024)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstring>
+#include <utility>
+#include <limits>
+
 
 /*
     I want to create an a simple tic-tac-toe application
     1) Allows UI for the game
     2) Basic rules
-
 */
 
 static void setBoard(char board[3][3])
@@ -19,26 +21,78 @@ static void setBoard(char board[3][3])
     }
 }
 
+char inputValidationPlayer(char &c){
+    while(1){
+        if(std::cin >> c){
+            if(std::cin.peek() == '\n'){
+                break;
+            }else{
+                std::cout << "Too many characters entered. Please enter a single character: ";
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear the buffer
+            }
+        }else{
+            std::cout << "Enter in a valid character: " <<std::endl;
+            // Clear the buffer (https://stackoverflow.com/questions/2075898/good-input-validation-loop-using-cin-c)
+            std::cin.clear(); // clears the error flag on cin (so that future I/O operations will work correctly)
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore until \n
+        }
+    }
+    return c;
+}
+
+
+/*
+    Conditions for winning:
+    1) 
+*/
+bool checkWinner(char board[3][3])
+{
+    for(int i = 0; i < 3; i++){
+        for(int k = 0; k < 3; k++)
+        {
+            
+        }
+    }
+}
+
+
 int main()
 {
     char playerOne, playerTwo;
     char board [3][3] = {
-        {'a',' ',' '},{' ',' ',' '},{' ',' ',' '},
+        {' ',' ',' '},{' ',' ',' '},{' ',' ',' '},
     };
     std::cout<<"Welcome to Tic-Tac-Toe!\n";
-    std::cout<<"For player one, input the symbol you wish to play with: ";
-    std::cin>>playerOne;
-    std::cout<<"For player two, input the symbol you wish to play with: ";
+    // Get symbol for player One and Two
+    std::cout<<"\nFor player one, input the symbol you wish to play with: ";
+    inputValidationPlayer(playerOne);
 
-    // Check for whether symbols for playerOne vs playerTwo are equivalent
-    while(std::cin>>playerTwo){
-        if(playerOne == playerTwo){
-            std::cout<<"Please enter a different symbol: ";
-            continue;
-        }else{
-            break;
+    std::cout<<"\nFor player two, input the symbol you wish to play with: ";
+    while(inputValidationPlayer(playerTwo) == playerOne){
+        std::cout<<"Please enter another symbol: ";
+    }
+
+    // Convert players into array
+    char players [2] = {playerOne, playerTwo};
+
+    setBoard(board);
+    
+    // Maximum number of turns possible for a 3x3 tic tac toe
+    for(int i = 0; i < 9; i++){
+        // Player one and player two
+        for(int k = 0; k < 2; k++){
+            int row, col; // Temporary column and row variables
+            std::cout<<"\nChoose coordinates: " << std::endl;
+            std::cout<<"Row for player "<< k+1 <<": ", std::cin>>row;
+            std::cout<<"Column for player "<< k+1 <<": ", std::cin>>col;
+            board[row-1][col-1] = players[k];
+            setBoard(board);
+
+            
         }
     }
-    setBoard(board);
+    
+
+   return 0;
 }
 
